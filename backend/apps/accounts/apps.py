87bd2529be8481b2912a14e6dfe8f9e7fa3b7b67
nigdgs <ioper5612@gmail.com>
2025-10-21 11:08:26 +0300

    Revert "update admin panel"
    This reverts commit 0b819030742ac0b9ed213ae871bacb8606018923.

--- backend/apps/accounts/apps.py
+++ backend/apps/accounts/apps.py
@@ -4,7 +4,6 @@ from django.apps import AppConfig
 class AccountsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "apps.accounts"
-    verbose_name = "Учётные записи"
 
     def ready(self):
         from . import signals  # noqa
--- backend/apps/devices/apps.py
+++ backend/apps/devices/apps.py
@@ -1,7 +1,1 @@
-from django.apps import AppConfig
-
-
-class DevicesConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "apps.devices"
-    verbose_name = "Устройства"
+# TODO
